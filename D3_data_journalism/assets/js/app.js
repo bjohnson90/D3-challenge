20f661fb10eeb9f27a0072468cd74bee6b5f694e
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-30 22:47:03 -0500

    added the on click handler and the rest of the stubbed out functions just need to finish out the rest of the stuff

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -51,6 +51,19 @@ function get_yScale(censusData, currYAxis) {
     return null
 }
 
+function renderXAxis(newXScale, xAxis) {
+	return null
+}
+
+// function used for updating yAxis var upon click on axis label
+function renderYAxis(newYScale, yAxis) {
+    return null
+}
+
+function renderCircles(circles, newXScale, newYScale, currXAxis, currYAxis){
+    return null
+}
+
 function updateCircles(currXAxis, currYAxis, circles) {
     return null
 }
@@ -72,8 +85,8 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
 	var xScale = get_xScale(censusData, currXAxis);
 	var yScale = get_yScale(censusData, currYAxis)
 
-	var leftAxis = d3.axisLeft(yLinearScale);
-	var bottomAxis = d3.axisBottom(xLinearScale);
+	var leftAxis = d3.axisLeft(yScale);
+	var bottomAxis = d3.axisBottom(xScale);
 
 	var xAxis = charts.append("g").classed("x-axis", true)
         .attr("transform", `translate(0, ${height})`)
@@ -113,6 +126,52 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
         .text(xAxes[i].label));
 
     var circles = updateCircles(currXAxis, currYAxis, circles);
+
+    labels.selectAll("text")
+    .on("click", function () {
+        // get selection
+        var value = d3.select(this).attr("value");
+        var isXAxis = false;
+        // update value
+        for (var i = 0; i < xAxes.length; i++)
+            if (xAxes[i].option === value) {
+                isXAxis = true;
+                break;
+            }
+        // dont do unless it actually changed
+        if ((isXAxis && value !== currXAxis) || (isXAxis === false && value !== currYAxis)) {
+
+            if (isXAxis) {
+                currXAxis = value
+            }
+            else {
+                currYAxis = value;
+            }
+            //refresh the x and y scales
+            xScale = get_xScale(censusData, currXAxis);
+            yScale = get_yScale(censusData, currYAxis);
+
+            // re-render x and y scales
+            xAxis = renderXAxis(xScale, xAxis);
+            yAxis = renderYAxis(yScale, yAxis);
+
+            // updates circles with new x and y values
+            circles = renderCircles(circles, xScale, yScale, currXAxis, currYAxis);
+
+            // updates tooltips with new info
+            circles = updateCircles(currXAxis, currYAxis, circles);
+
+            // switch up classes to emphasize active x and y
+            for (var i = 0; i < xAxes.length; i++)
+                xLabels[i]
+                    .classed("active", currXAxis === xAxes[i].option)
+                    .classed("inactive", currXAxis !== xAxes[i].option);
+            for (var i = 0; i < yAxes.length; i++)
+                yLabels[i]
+                    .classed("active", currYAxis === yAxes[i].option)
+                    .classed("inactive", currYAxis !== yAxes[i].option);
+        }
+    });
 
 }).catch(function (error) {
 	console.log(error);
