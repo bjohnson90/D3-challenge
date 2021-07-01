9a19a16cff6e376a6f40453300e3465059029a88
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-30 21:26:52 -0500

    read the csv into d3 and catch errors; did the first part of the svg initialization

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,61 @@
-// @TODO: YOUR CODE HERE!
+var xAxes = [
+	{
+		option: "poverty",
+		label: "In Poverty (%)"
+	},
+	{
+		option: "age",
+		label: "Age (Median)"
+	},
+	{
+		option: "income",
+		label: "Household Income (USD Median)"
+	}
+];
+var yAxes = [
+	{
+		option: "healthcare",
+		label: "Lacks Healthcare (%)"
+	},
+	{
+		option: "smokes",
+		label: "Smokes (%)"
+	},
+	{
+		option: "obesity",
+		label: "Obese (%)"
+	}
+];
+
+var currXAxis = xAxes[0].option;
+var currYAxis = yAxes[0].option;
+
+var width = 960 - 100 - 40;
+var height = 500 - 20 - 80;
+
+var svg = d3
+	.select("#scatter")
+	.append("svg")
+	.attr("width", 960)
+	.attr("height", 500);
+
+var charts = svg.append("g")
+	.attr("transform", `translate(${100}, ${20})`);
+
+
+d3.csv("assets/data/data.csv").then(function (censusData, err) {
+	if (err) throw err;
+
+	censusData.forEach(function (data) {
+		data.id = +data.id;
+		data.poverty = +data.poverty;
+		data.age = +data.age;
+		data.income = +data.income;
+		data.healthcare = +data.healthcare;
+		data.obesity = +data.obesity;
+		data.smokes = +data.smokes;
+	});
+
+}).catch(function (error) {
+	console.log(error);
+});
