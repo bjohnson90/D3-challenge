b2b4b195397beb38bc757d3228fde50e216cc072
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-30 22:32:26 -0500

    made more progress and stubbed out some functions that I need to finish up later

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -42,10 +42,22 @@ var svg = d3
 var charts = svg.append("g")
 	.attr("transform", `translate(${100}, ${20})`);
 
+function get_xScale(censusData, currXAxis) {
+    return null
+}
+
+// function used for updating y-scale var upon click on axis label
+function get_yScale(censusData, currYAxis) {
+    return null
+}
+
+function updateCircles(currXAxis, currYAxis, circles) {
+    return null
+}
 
 d3.csv("assets/data/data.csv").then(function (censusData, err) {
 	if (err) throw err;
-
+    //get data
 	censusData.forEach(function (data) {
 		data.id = +data.id;
 		data.poverty = +data.poverty;
@@ -55,6 +67,52 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
 		data.obesity = +data.obesity;
 		data.smokes = +data.smokes;
 	});
+
+	// stubbed out for now
+	var xScale = get_xScale(censusData, currXAxis);
+	var yScale = get_yScale(censusData, currYAxis)
+
+	var leftAxis = d3.axisLeft(yLinearScale);
+	var bottomAxis = d3.axisBottom(xLinearScale);
+
+	var xAxis = charts.append("g").classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+	var yAxis = charts.append("g").classed("y-axis", true).call(leftAxis);
+    var circles = charts.selectAll("g>circle").data(censusData).enter().append("g");
+
+    var labels = charts.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var xLabels = [];
+	var yLabels = [];
+
+    circles.append("circle")
+    .attr("cx", d => xLinearScale(d[currXAxis]))
+    .attr("cy", d => yLinearScale(d[currYAxis]))
+    .attr("r", svgCirclesRadius)
+    .attr("fill", svgCirclesColor);
+
+    circles.append("text")
+    .attr("x", d => xLinearScale(d[currXAxis]))
+    .attr("y", d => yLinearScale(d[currYAxis]))
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "middle")
+    .attr("font-size", `${svgCirclesRadius}px`)
+    .attr("fill", "white")
+    .text(d => d.abbr);
+
+    for (var i = 0; i < xAxes.length; i++)
+    xLabels.push(labels.append("text")
+        .attr("x", 0)
+        .attr("y", (i + 1) * 17)
+        .attr("value", xAxes[i].option)
+        .classed("active", xAxes[i].option === currXAxis)
+        .classed("inactive", xAxes[i].option !== currXAxis)
+        .text(xAxes[i].label));
+
+    var circles = updateCircles(currXAxis, currYAxis, circles);
 
 }).catch(function (error) {
 	console.log(error);
