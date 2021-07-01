21bbda862f6e9030a144c0795c98ffa0596629e1
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-30 23:34:00 -0500

    got it working

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -26,12 +26,10 @@ var yAxes = [
 		label: "Obese (%)"
 	}
 ];
-
 var currXAxis = xAxes[0].option;
 var currYAxis = yAxes[0].option;
-
-var width = 960 - 100 - 40;
-var height = 500 - 20 - 80;
+var width = 820; 
+var height = 400;
 
 var svg = d3
 	.select("#scatter")
@@ -43,29 +41,96 @@ var charts = svg.append("g")
 	.attr("transform", `translate(${100}, ${20})`);
 
 function get_xScale(censusData, currXAxis) {
-    return null
+    return d3.scaleLinear()
+        .domain([d3.min(censusData, d => d[currXAxis]) * (1 - 0.2),
+        d3.max(censusData, d => d[currXAxis]) * (1 + 0.2)])
+        .range([0, width]);
 }
 
-// function used for updating y-scale var upon click on axis label
 function get_yScale(censusData, currYAxis) {
-    return null
+	return d3.scaleLinear()
+		.domain([0, d3.max(censusData, d => d[currYAxis]) * (1 + 0.2)])
+		.range([height, 0]);
 }
 
-function renderXAxis(newXScale, xAxis) {
-	return null
+function renderXAxis(newXAxis, xAxis) {
+	var bottomAxis = d3.axisBottom(newXAxis);
+	xAxis.transition()
+		.duration(1000)
+		.call(bottomAxis);
+	//
+	return xAxis;
 }
 
 // function used for updating yAxis var upon click on axis label
-function renderYAxis(newYScale, yAxis) {
-    return null
+function renderYAxis(newYAxis, yAxis) {
+	var leftAxis = d3.axisLeft(newYAxis);
+	// shift axis
+	yAxis.transition()
+		.duration(1000)
+		.call(leftAxis);
+	//
+	return yAxis;
 }
 
-function renderCircles(circles, newXScale, newYScale, currXAxis, currYAxis){
-    return null
+function renderCircles(circles, newXAxis, newYAxis, currXAxis, currYAxis){
+	circles.selectAll("circle")
+		.transition()
+		.duration(1000)
+		.attr("cx", d => newXAxis(d[currXAxis]))
+		.attr("cy", d => newYAxis(d[currYAxis]));
+	// use a transition to shift the circles' text
+	circles.selectAll("text")
+		.transition()
+		.duration(1000)
+		.attr("x", d => newXAxis(d[currXAxis]))
+		.attr("y", d => newYAxis(d[currYAxis]));
+	//
+	return circles;
+}
+
+function formatToolTipText(label, number) {
+	var line = `${label.split(" (")[0]}: `;
+	if (label.includes("%")) {
+		line += `${number}%`
+    }
+	else if (label.includes("USD")) {
+		line += `$${number.toLocaleString()}`;
+    }
+	else {
+        line += number;
+    }
+	return line;
 }
 
 function updateCircles(currXAxis, currYAxis, circles) {
-    return null
+	var xLabel = "";
+	var yLabel = "";
+	// loop thru the x axes to get the matching option
+	for (var i = 0; i < xAxes.length; i++)
+		if (currXAxis == xAxes[i].option)
+			xLabel = xAxes[i].label;
+	// loop thru the y axes to get the matching option
+	for (var i = 0; i < yAxes.length; i++)
+		if (currYAxis == yAxes[i].option)
+			yLabel = yAxes[i].label;
+	// add a tooltip
+	var toolTip = d3.tip()
+		.attr("class", "tooltip")
+		.offset([110, 0])
+		.html(d => [
+			d.state,
+			formatToolTipText(xLabel, d[currXAxis]),
+			formatToolTipText(yLabel, d[currYAxis])
+		].join("<br>"));
+	// add the callback
+	circles.call(toolTip);
+	// add the mouse over event
+	circles
+		.on("mouseover", (data, index, element) => toolTip.show(data, element[index]))
+		.on("mouseout", (data, index, element) => toolTip.hide(data, element[index]));
+	//
+	return circles;
 }
 
 d3.csv("assets/data/data.csv").then(function (censusData, err) {
@@ -81,7 +146,6 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
 		data.smokes = +data.smokes;
 	});
 
-	// stubbed out for now
 	var xScale = get_xScale(censusData, currXAxis);
 	var yScale = get_yScale(censusData, currYAxis)
 
@@ -102,17 +166,17 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
 	var yLabels = [];
 
     circles.append("circle")
-    .attr("cx", d => xLinearScale(d[currXAxis]))
-    .attr("cy", d => yLinearScale(d[currYAxis]))
-    .attr("r", svgCirclesRadius)
-    .attr("fill", svgCirclesColor);
+    .attr("cx", d => xScale(d[currXAxis]))
+    .attr("cy", d => yScale(d[currYAxis]))
+    .attr("r", 10)
+    .attr("fill", "#E6E6FA");
 
     circles.append("text")
-    .attr("x", d => xLinearScale(d[currXAxis]))
-    .attr("y", d => yLinearScale(d[currYAxis]))
+    .attr("x", d => xScale(d[currXAxis]))
+    .attr("y", d => yScale(d[currYAxis]))
     .attr("text-anchor", "middle")
     .attr("dominant-baseline", "middle")
-    .attr("font-size", `${svgCirclesRadius}px`)
+    .attr("font-size", `${10}px`)
     .attr("fill", "white")
     .text(d => d.abbr);
 
@@ -124,6 +188,16 @@ d3.csv("assets/data/data.csv").then(function (censusData, err) {
         .classed("active", xAxes[i].option === currXAxis)
         .classed("inactive", xAxes[i].option !== currXAxis)
         .text(xAxes[i].label));
+
+    for (var i = 0; i < yAxes.length; i++)
+		yLabels.push(labels.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("x", height * 0.5)
+			.attr("y", -width * 0.5 - (i + 2) * 17)
+			.attr("value", yAxes[i].option)
+			.classed("active", yAxes[i].option === currYAxis)
+			.classed("inactive", yAxes[i].option !== currYAxis)
+			.text(yAxes[i].label));
 
     var circles = updateCircles(currXAxis, currYAxis, circles);
 
